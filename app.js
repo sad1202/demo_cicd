0ef65954e052aa8c776534210c098b2340833aff
hieusieucup <hieuspace1202@gamil.com>
2026-06-02 17:50:52 +0700

    hieu

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ function add(a, b) {
 function subtract(a, b) {
     return a - b;
 }
-if (typeof document !== 'undefined') {
-    const ketQua = add(5, 7);
-    document.getElementById('math-result').innerText = `Kết quả phép tính 5 + 7 là: ${ketQua}`;
-}
+// if (typeof document !== 'undefined') {
+//     const ketQua = add(5, 7);
+//     document.getElementById('math-result').innerText = `Kết quả phép tính 5 + 7 là: ${ketQua}`;
+// }
 
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = { add, subtract };
