88f08f61e9a6c3c9bb10ad38094978b32d0b08e7
hieusieucup <hieuspace1202@gamil.com>
2026-06-02 16:03:34 +0700

    hieu

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const { add, subtract } = require('./app.js');
 
 test('Kiểm tra hàm add: 5 + 7 phải bằng 12', () => {
     const result = add(5, 7);
-    assert.strictEqual(result, 12);
+    assert.strictEqual(result, 11);
 });
 test('check', () => {
     const result = subtract(10, 4);
