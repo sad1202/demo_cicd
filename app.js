8d22027624b2ca12414bc0a8b7fd585a9b87307a
hieusieucup <hieuspace1202@gamil.com>
2026-06-02 13:54:48 +0700

    ih

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 function add(a, b) {
     return a + b;
 }
-
+function subtract(a, b) {
+    return a - b;
+}
 if (typeof document !== 'undefined') {
     const ketQua = add(5, 7);
     document.getElementById('math-result').innerText = `Kết quả phép tính 5 + 7 là: ${ketQua}`;
 }
 
 if (typeof module !== 'undefined' && module.exports) {
-    module.exports = { add };
+    module.exports = { add, subtract };
 }
--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 const test = require('node:test');
 const assert = require('node:assert');
-const { add } = require('./app.js');
+const { add, subtract } = require('./app.js');
 
 test('Kiểm tra hàm add: 5 + 7 phải bằng 12', () => {
     const result = add(5, 7);
     assert.strictEqual(result, 12);
 });
+test('check', () => {
+    const result = subtract(10, 4);
+    assert.strictEqual(result, 6);
+})
