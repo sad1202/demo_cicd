2811d7bac2ee2051b6dcb9b4ab09c8264d9e23ca
sad1202 <145451527+sad1202@users.noreply.github.com>
2026-06-05 02:26:38 +0000

    Deploying to gh-pages from @ sad1202/demo_cicd@9f1dcd757235de4e8b1e57bdc6a2a1bfda995dc5 🚀

---
+++ staging/env-config.js
@@ -0,0 +1,1 @@
+<script>window.ENV_NAME='STAGING (DELIVERY)'; window.ENV_COLOR='#e67e22';</script>
---
+++ staging/test.js
@@ -0,0 +1,12 @@
+const test = require('node:test');
+const assert = require('node:assert');
+const { add, subtract } = require('./app.js');
+
+test('5 + 7 = 12', () => {
+    const result = add(5, 7);
+    assert.strictEqual(result, 12);
+});
+test('10 - 4 = 6', () => {
+    const result = subtract(10, 4);
+    assert.strictEqual(result, 6);
+});
